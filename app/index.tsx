6049d67bbe4505d5f8c567d98553be5b2f9b3e2c
bcebel <bcebel@gmail.com>
2024-12-27 15:27:51 -0600

    gh pages

---
+++ app/index.tsx
@@ -0,0 +1,45 @@
+
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function Index() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api") // Replace with your backend URL
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>{message || "Loading..!."}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+});
+/*
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+  */
