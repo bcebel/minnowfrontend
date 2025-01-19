6cb455e1432da3d76305276191105883cac8457c
bcebel <bcebel@gmail.com>
2025-01-19 14:28:32 -0600

    youtube

--- app/(tabs)/Ampcomponent.tsx
+++ app/(tabs)/Ampcomponent.tsx
@@ -1,9 +1,22 @@
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
-export default function App() {
-  return (
+// Define the App function here, but only export once at the end
+let App;
+
+if (Platform.OS === "web") {
+  // Code specific for the web platform
+  App = () => (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://minnowspace.com/yotu.html" height=1000</iframe>"`,
+      }}
+    />
+  );
+} else if (Platform.OS === "ios" || Platform.OS === "android") {
+  // Code specific for the mobile platform (iOS or Android)
+  App = () => (
     <WebView
       style={styles.container}
       source={{ uri: "https://minnowspace.com/yotu.html" }}
@@ -17,3 +30,6 @@ const styles = StyleSheet.create({
     marginTop: Constants.statusBarHeight,
   },
 });
+
+// Export the App function at the end
+export default App;
