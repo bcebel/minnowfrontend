fadb39ef4f0f449f1f86d7fe599f1912ff35f37e
bcebel <bcebel@gmail.com>
2025-02-12 00:47:36 -0600

    web

--- app/(tabs)/torrent.js
+++ app/(tabs)/torrent.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { WebView } from "react-native-webview";
+  
+import React, { useEffect } from "react";
+
+import { Platform, View, Text, Linking } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
+  const isWeb = Platform.OS === 'web';
+
+  if (isWeb) {
+        window.location.href = "https://webtorrent.io";
+    // For web, redirect to the hosted site
+  }
+
+  // For mobile, use WebView
   return (
     <WebView
-      source={{ uri: "https://webtorrent.io/" }} // Example WebTorrent demo
+      source={{ uri: 'https://webtorrent.io' }}
       style={{ flex: 1 }}
     />
   );
