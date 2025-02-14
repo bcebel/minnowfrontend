165e3d34c1b3b481739ca2d1e834c15fcbb060a0
bcebel <bcebel@gmail.com>
2025-02-14 12:50:19 -0600

    torrent

--- app/(tabs)/torrent.js
+++ app/(tabs)/torrent.js
@@ -3,21 +3,16 @@ import React, { useEffect } from "react";
 
 import { Platform, View, Text, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
-
+  const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 
 export default function App() {
   const isWeb = Platform.OS === 'web';
 
   if (isWeb) {
-        window.location.href = "https://webtorrent.io";
+        window.location.href = BACKEND_URL;
     // For web, redirect to the hosted site
   }
 
   // For mobile, use WebView
-  return (
-    <WebView
-      source={{ uri: 'https://webtorrent.io' }}
-      style={{ flex: 1 }}
-    />
-  );
+  return <WebView source={{ uri: BACKEND_URL }} style={{ flex: 1 }} />;
 }
