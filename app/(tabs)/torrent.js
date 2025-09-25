ef60daeec1c7c0531648a5447ff70acf14d237c0
Brian Ebel <120604672+bcebel@users.noreply.github.com>
2025-09-25 14:14:22 -0500

    Update torrent.js

--- app/(tabs)/torrent.js
+++ app/(tabs)/torrent.js
@@ -1,19 +1,34 @@
   
 import React, { useEffect } from "react";
 
+
+
 import { Platform, View, Text, Linking } from 'react-native';
+
 import { WebView } from 'react-native-webview';
-  const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+
+  const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+
+
 
 export default function App() {
-  // const isWeb = Platform.OS === 'web'; // <-- Can remove this line
 
-  // if (isWeb) { // <-- Remove this
-  //    window.location.href = BACKEND_URL; // <-- Remove this
-  //    // For web, redirect to the hosted site // <-- Remove this
-  // } // <-- Remove this
+  const isWeb = Platform.OS === 'web';
 
-  // Now, for ALL platforms, it will attempt to render the WebView
-  // Note: react-native-webview on 'web' renders as a standard iframe.
-  return <WebView source={{ uri: BACKEND_URL }} style={{ flex: 1 }} />;
+
+
+  if (isWeb) {
+
+        window.location.href = BACKEND_URL;
+
+    // For web, redirect to the hosted site
+
+  }
+
+
+
+  // For mobile, use WebView
+
+  return <WebView source={{ uri: BACKEND_URL }} style={{ flex: 1 }} />;
+
 }
