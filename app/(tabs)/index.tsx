b13ba9824a458fb147109545b0f13e92394c3614
Brian Ebel <120604672+bcebel@users.noreply.github.com>
2025-11-26 12:22:49 -0600

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -62,8 +62,7 @@ export default function HomeScreen() {
           </TouchableOpacity>
         </View>
                        <ImageBackground
-          source={require("@/assets/images/pexels-pixabay-270873.jpg") resizeMode="cover" style={{ flex: 1 }}
-        >
+          source={require("@/assets/images/pexels-pixabay-270873.jpg")} resizeMode="cover" style={{ flex: 1 }}>
         <View
           style={styles.features} >
           {/* Your feature items */}
