2b0f07894b6d9686fc8b8c007dc9f1ee14666ff8
bcebel <bcebel@gmail.com>
2026-01-01 16:00:48 -0600

    remove maybe

--- components/NeighborhoodLiveStreamPlayer.jsx
+++ components/NeighborhoodLiveStreamPlayer.jsx
@@ -47,7 +47,7 @@ class StreamController {
     this.streamingAllowed = true;
     this.nextIndex = 0;
     this.chunkBuffer = new Map();
-    this.maxBufferSize = 30;
+    this.maxBufferSize = 50;
     this.bufferStartTimes = new Map();
 
     this.trackers = [
@@ -234,7 +234,6 @@ class StreamController {
             `✅ Downloaded chunk ${this.nextIndex}, size: ${buf.length} bytes`
           );
 
-          this.maybeCleanupBuffers();
 
           // Append to SourceBuffer
           this.sb.appendBuffer(buf);
