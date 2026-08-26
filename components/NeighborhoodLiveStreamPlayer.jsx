a4421ab6bdab8ad9c81de72a84e229cb66f18930
bcebel <bcebel@gmail.com>
2026-08-25 21:29:00 -0500

    Things

--- components/NeighborhoodLiveStreamPlayer.jsx
+++ components/NeighborhoodLiveStreamPlayer.jsx
@@ -783,6 +783,7 @@ export default function NeighborhoodLiveStreamPlayer({
 const styles = StyleSheet.create({
   container: {
     width: "100%",
+    height: "100%",
     aspectRatio: 1, // Square for now
     backgroundColor: "#000",
     position: "relative",
