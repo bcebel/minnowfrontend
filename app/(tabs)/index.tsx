0e0b895cad883629a3a2ad3c48d70015d3838895
Brian Ebel <120604672+bcebel@users.noreply.github.com>
2026-09-03 18:21:39 -0500

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   secondaryButton: {
-    borderWidth: 2,
+    borderWidth: 0.5,
     paddingVertical: 16,
     paddingHorizontal: 20,
     borderRadius: 48,
