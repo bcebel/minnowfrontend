178fc923ffb46a616495017e8bb487b1e56f4b94
Brian Ebel <120604672+bcebel@users.noreply.github.com>
2026-09-03 22:06:26 -0500

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -164,7 +164,7 @@ export default function TabLayout() {
 const styles = StyleSheet.create({
   bubbleGlass: {
     // The "bubble" look
-    backgroundColor: 'rgba(255, 0, 129, 0.3)',
+    backgroundColor: 'rgba(255, 0, 129, 0.1)',
     borderWidth: 1,
     borderColor: 'rgba(255, 0, 129, 0.3)',
     borderRadius: 48, // Fully round
