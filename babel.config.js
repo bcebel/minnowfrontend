9ddb8324f91dabbc9c219779dc14aeaa8b936236
bcebel <bcebel@gmail.com>
2026-09-10 14:11:11 -0500

    remove console logs

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-
+    plugins: [
+      // Only strip in production
+      ...(process.env.NODE_ENV === "production"
+        ? ["transform-remove-console"]
+        : []),
+    ],
   };
 };
